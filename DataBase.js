fb22a2e774e6d89eace6f39ca6ddbf920ac054ac
AndrewKysiliuk <andriy.av95@ukr.net>
2018-11-26 00:14:52 +0200

    Server for final work

--- DataBase.js
+++ DataBase.js
@@ -6,73 +6,191 @@ module.exports = class DataBase {
     constructor(){
         this._record = fs.readFileSync(dbPath, 'utf-8');
         this._data = JSON.parse(this._record);
+        this._users = this._data.users;
+        this._usersData = this._data.usersData;
+        this._defaultCategory = [
+            {
+                "catName": "alco",
+                "title" : "Алкогольні напої",
+            },
+            {
+                "catName": "noAlco",
+                "title" : "Безалкогольні напої",
+            },
+            {
+                "catName": "salad",
+                "title" : "Салати",
+            },
+            {
+                "catName": "first",
+                "title" : "Перші страви",
+            },
+            {
+                "catName": "sup",
+                "title" : "Супи",
+            },
+            {
+                "catName": "pizza",
+                "title" : "Піца",
+            },
+            {
+                "catName": "sushi",
+                "title" : "Суші",
+            }
+        ];
     }
 
 
-    get(id) {
+    getData(key, category, index) {
         return new Promise((resolve, reject) =>{
-            if(id){
-                let item = this._data.gallery.find(item => item.id === Number(id));
-                if(item){
-                    resolve(item);
+            let keyData = this._usersData.find(item => item.key === key);
+            if(keyData){
+                if(category){
+                    let needCategory = keyData.categories.find(item => item.catName === category);
+                    if (needCategory){
+                        if(index){
+                            let needItem = needCategory.data.find(item => item.id === Number(index));
+                            if (needItem){
+                                resolve(needItem)
+                            } else {
+                                reject('Item not found')
+                            }
+                        } else {
+                            resolve(needCategory.data);
+                        }
+                    }
+                    else {
+                        reject('Category not found');
+                    }
+                } else {
+                    let categoryList = [];
+                    keyData.categories.forEach(item => categoryList.push(
+                        {
+                            "category": item.catName,
+                            "title": item.title
+                        }));
+                    if(categoryList.length) {
+                        resolve(categoryList);
+                    } else{
+                        reject('no category')
+                    }
+                }
+            } else {
+                reject('Invalid key');
+            }
+        });
+    }
+
+    authorization(item) {
+        return new Promise((resolve, reject) => {
+            let user = this._users.find(user => user.email === item.email);
+            if(user){
+                if(user.password === item.password){
+                    resolve(`${user.email}${user.password}`);
+                } else {
+                    reject('Invalid password');
+                }
+            } else {
+                reject('User not found');
+            }
+        });
+    }
+
+    registration(item) {
+        return new Promise((resolve, reject) => {
+            if(this._users.find(user => user.email === item.email)) {
+                reject(`User with email: ${item.email} already exists`);
+            }
+            else {
+                let userData = {
+                    'key': `${item.email}${item.password}`,
+                    'categories': this._defaultCategory
+                };
+                this._usersData.push(userData);
+                this._users.push(item);
+
+                fs.writeFile(dbPath, JSON.stringify(this._data),'utf-8', () => {
+                    console.log('New user was create');
+                    resolve('New user was create');
+                });
+            }
+        });
+    }
+
+    post(key, category, item){
+        return new Promise((resolve, reject) => {
+            let keyData = this._usersData.find(item => item.key === key);
+            if(keyData) {
+                let needCategory = keyData.categories.find(item => item.catName === category);
+                if (needCategory) {
+                    needCategory.data.unshift(item);
+                    fs.writeFile(dbPath, JSON.stringify(this._data),'utf-8', () => {
+                        console.log('New item was write');
+                        resolve('New item was write');
+                    });
+                }
+            }
+        });
+    }
+
+    put(key, category, delIndex, editItem) {
+        return new Promise(((resolve, reject) => {
+            let keyData = this._usersData.find(item => item.key === key);
+            if(keyData){
+                let needCategory = keyData.categories.find(item => item.catName === category);
+                if (needCategory){
+                    let id = -1;
+                    needCategory.data.find((item, index) => {
+                        if(item.id === Number(delIndex)){
+                            id = index;
+                        }
+                    });
+                    if(id >= 0) {
+                        needCategory.data.splice(id,1,editItem);
+                        fs.writeFile(dbPath,JSON.stringify(this._data),'utf-8', () => {
+                            resolve('Item was edited');
+                        });
+                    }
+                    else {
+                        reject('Item not found');
+                    }
                 }else {
-                    reject();
+                    reject('Category not found');
                 }
             }else {
-                resolve(this._data.gallery);
-            }
-        });
-
-
-    }
-
-    post(item){
-        return new Promise(resolve => {item.id = this._data.gallery.length+1;
-            this._data.gallery.push(item);
-            fs.writeFile(dbPath,JSON.stringify(this._data),'utf-8', () => {
-                console.log('New item was write');
-                resolve();
-            });
-        })
-    }
-
-    put(editItem) {
-        return new Promise((resolve, reject) =>{
-            let id;
-            this._data.gallery.find((item, index) => {
-                if(item.id === editItem.id){
-                    id = index;
-                }
-            } );
-            if(id >= 0) {
-                this._data.gallery.splice(id,1,editItem);
-                fs.writeFile(dbPath,JSON.stringify(this._data),'utf-8', () => { console.log('Item was edited')});
-                resolve();
-            }
-            else {
-                reject();
-            }
-        });
-    }
-
-    del(delId){
-        return new Promise(((resolve, reject) => {
-            let id;
-            this._data.gallery.find((item, index) => {
-                if(item.id === Number(delId)){
-                    id = index;
-                }
-            } );
-            if(id >= 0) {
-                this._data.gallery.splice(id,1);
-                fs.writeFile(dbPath,JSON.stringify(this._data),'utf-8', () => { console.log('Item was deleted')});
-                resolve();
-            }
-            else {
-                reject();
+                reject('Invalid key');
             }
         }));
+    }
 
+    del(key, category, delIndex){
+        return new Promise(((resolve, reject) => {
+            let keyData = this._usersData.find(item => item.key === key);
+            if(keyData){
+                let needCategory = keyData.categories.find(item => item.catName === category);
+                if (needCategory){
+                    let id = -1;
+                    needCategory.data.find((item, index) => {
+                        if(item.id === Number(delIndex)){
+                            id = index;
+                        }
+                    });
+                    if(id >= 0) {
+                        needCategory.data.splice(id,1);
+                        fs.writeFile(dbPath,JSON.stringify(this._data),'utf-8', () => {
+                            resolve('Item was deleted');
+                        });
+                    }
+                    else {
+                        reject('Item not found');
+                    }
+                }else {
+                    reject('Category not found');
+                }
+            }else {
+                reject('Invalid key');
+            }
+        }));
     }
 
 };
--- index.js
+++ index.js
@@ -6,47 +6,61 @@ const app = express();
 const port = 3000;
 const base = new DataBase();
 
-// app.use((req, res, next) =>{
-//     res.header('Access-Control-Allow-Origin','*');
-//     next();
-// });
 app.use(cors());
 app.use(express.json());
 
-app.get('/gallery', (req, res) => {
-    base.get().then(resolve => res.status(200).send(resolve));
-});
-app.get('/gallery/:id', (req, res) => {
-    base.get(req.params.id).then(
+app.get('/home/:key', (req, res) => {  // отримуємо список категорій
+    base.getData(req.params.key).then(
         resolve => res.status(200).send(resolve),
-        reject => res.status(404).send({})
+        reject => res.status(404).send(reject)
+    );
+
+});
+app.get('/home/:key/:category', (req, res) => { // отримуємо масив категорій
+    base.getData(req.params.key, req.params.category).then(
+        resolve => res.status(200).send(resolve),
+        reject => res.status(404).send(reject)
+    );
+
+});
+app.get('/home/:key/:category/:id', (req, res) => {// отримуємо запис по id з вказаної категорії
+    base.getData(req.params.key, req.params.category, req.params.id).then(
+        resolve => res.status(200).send(resolve),
+        reject => res.status(404).send(reject)
+    );
+
+});
+
+app.post('/authorization', (req, res) => {
+    base.authorization(req.body).then(
+        resolve => res.status(200).send(JSON.stringify(resolve)),
+        reject => res.status(401).send(JSON.stringify(reject))
+    );
+});
+app.post('/registration', (req, res) => {
+    base.registration(req.body).then(
+        resolve => res.status(200).send(resolve),
+        reject => res.status(404).send(reject)
     );
 });
 
-app.post('/gallery', (req, res) => {
-    base.post(req.body).then(() => res.status(200).send({}));
-});
-
-app.put('/gallery/:id', (req, res) =>{
-    base.put(req.body).then(
-        resolve => res.status(200).send(),
-        reject =>{
-            res.status(404).send();
-            console.log('Error item not found!');
-        });
-});
-
-
-app.delete('/gallery/:id', (req, res) =>{
-    base.del(req.params.id).then(
+app.post('/home/:key/:category', (req, res) => {
+    base.post(req.params.key, req.params.category, req.body).then(
         resolve => res.status(200).send(resolve),
-        reject => {
-            res.status(404).send();
-            console.log(`Error item not found`);
-        });
+        reject => res.status(404).send(reject));
 });
 
+app.put('/home/:key/:category/:id', (req, res) =>{
+    base.put(req.params.key, req.params.category, req.params.id, req.body).then(
+        resolve => res.status(200).send(resolve),
+        reject => res.status(404).send(reject));
+});
 
+app.delete('/home/:key/:category/:id', (req, res) =>{
+    base.del(req.params.key, req.params.category, req.params.id).then(
+        resolve => res.status(200).send(resolve),
+        reject => res.status(404).send(reject));
+});
 
 app.listen(port, console.log(`Server on port ${port} is start`));
 
